63f2044b0e6522f6367e525fd38c2b08811746ab
shadikodehode <shadi.kodehode@gmail.com>
2025-12-15 09:12:44 +0100

    Calculator v2
    Simple calculator

--- Program.cs
+++ Program.cs
@@ -10,37 +10,35 @@
             string? op = Console.ReadLine(); //unknown null error, "?" after string was the solution
 
             Console.WriteLine("Enter second number");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            
-            double add = num1 + num2;
-            double sub = num1 - num2;
-            double mul = num1 * num2;            
-            double div = num1 / num2;
-            // sets the reference for the operators
-
-            if (op  == "/" && num1 == 0 || num2 == 0)
-            {
-                Console.WriteLine("Error, cannot divide by 0");
-            }
-            // checks if any of the numbers are 0 when using division, gives an error message if true
+            double num2 = Convert.ToDouble(Console.ReadLine());          
 
             if (op == "+")            
             {
+                double add = num1 + num2;
                 Console.WriteLine("Result:" + num1 + "+" + num2 + "="+ add);        
             }
             if (op == "-")            
             {
+                double sub = num1 - num2;
                 Console.WriteLine("Result:" + num1 + "-" + num2 + "="+ sub);         
             }
             if (op == "*")            
             {
+                double mul = num1 * num2; 
                 Console.WriteLine("Result:" + num1 + "-" + num2 + "="+ mul);    
             }               
             if (op == "/")
             {
-                Console.WriteLine("Result:" + num1 + "-" + num2 + "="+ div);
+                 if (num2 == 0)
+                {
+                    Console.WriteLine("Error, cannot divide by 0");
+                }else
+                {
+                    double div = num1 / num2;
+                    Console.WriteLine("Result:" + num1 + "-" + num2 + "="+ div);
+                }
             }
-            //checks which operator is used, and runs the  results based on the reference over
+            
                    
             
 
